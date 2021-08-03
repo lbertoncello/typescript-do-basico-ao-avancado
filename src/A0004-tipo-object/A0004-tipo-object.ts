fde046cd5f78db12fcb05957d7eebfa184f79543
lbertoncello <lucas_b_andrade@hotmail.com>
2021-08-03 18:33:12 -0300

    aula sobre tipo object

---
+++ src/A0004-tipo-object/A0004-tipo-object.ts
@@ -0,0 +1,25 @@
+const objetoA = {
+	chaveA: 'Valor A',
+	chaveB: 'Valor B',
+};
+
+objetoA.chaveA = 'Outro valor';
+// ERRO! Não é possível criar uma nova chave
+// objetoA.chaveC = 'Nova chave';
+
+const objetoB: {
+	chaveA: string;
+	chaveB: string;
+	chaveC?: string;
+	readonly chaveD: string;
+	[key: string]: unknown;
+} = {
+	chaveA: 'Valor A',
+	chaveB: 'Valor B',
+};
+
+objetoB.chaveA = 'Outro valor';
+objetoB.chaveC = 'Chave opcional';
+// ERRO! Não é possível alterar readonly
+objetoB.chaveD = 'Não é possível alterar';
+objetoB.chaveE = 'Nova chave';
