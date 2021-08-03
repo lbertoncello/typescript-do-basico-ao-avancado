8d8be20867a4065fc8a40768c9502c32a55d795d
lbertoncello <lucas_b_andrade@hotmail.com>
2021-08-03 18:56:33 -0300

    novos exemplos acrescentandos nas aulas sobre o tipo array

--- src/A0005-tipo-array/A0005-tipo-array.ts
+++ src/A0005-tipo-array/A0005-tipo-array.ts
@@ -15,6 +15,12 @@ const result = multiplicaArgs(1, 2, 3);
 const concatenacao = concatenaString('a', 'b', 'c');
 const upper = toUpperCase('a', 'b', 'c');
 
+// Array imutável
+const array1: readonly string[] = ['Lucas', 'Andrade'];
+const array2: ReadonlyArray<string> = ['Ana', 'Andrade'];
+
 console.log(result);
 console.log(concatenacao);
 console.log(upper);
+console.log(array1);
+console.log(array2);
