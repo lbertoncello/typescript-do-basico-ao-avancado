fed7e300bca28b74642806e86b4b456ebaa12099
lbertoncello <lucas_b_andrade@hotmail.com>
2021-08-12 12:19:13 -0300

    aula 06 sobre generics (type intersection)

---
+++ src/A0047-generics-6/A0047-generics-6.ts
@@ -0,0 +1,11 @@
+// Type intersection une dois tipos (ex.: O1 & O2)
+export function unirObjetos<O1, O2>(obj1: O1, obj2: O2): O1 & O2 {
+	// return Object.assign({}, obj1, obj2);
+	return { ...obj1, ...obj2 };
+}
+
+const obj1 = { chave1: 'valor 1' };
+const obj2 = { chave2: 'valor 2' };
+const uniao = unirObjetos(obj1, obj2);
+
+console.log(uniao);
