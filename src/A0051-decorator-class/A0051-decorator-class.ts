c9605b24981b8184654fc024755c4be29540edc5
lbertoncello <lucas_b_andrade@hotmail.com>
2021-08-16 17:53:00 -0300

    aula sobre decorator

---
+++ src/A0051-decorator-class/A0051-decorator-class.ts
@@ -0,0 +1,24 @@
+// Decorators são funções que modificam seu objeto/classe/etc. sem precisar alterar sua declaração
+@decorator
+export class Animal {
+	constructor(public nome: string, public cor: string) {}
+}
+
+function decorator<T extends new (...args: any[]) => any>(target: T) {
+	return class extends target {
+		cor: string;
+
+		constructor(...args: any[]) {
+			super(...args);
+			this.nome = this.inverte(args[0]);
+			this.cor = this.inverte(args[1]);
+		}
+
+		inverte(valor: string): string {
+			return valor.split('').reverse().join('');
+		}
+	};
+}
+
+const animal = new Animal('Macaco', 'roxo');
+console.log(animal);
